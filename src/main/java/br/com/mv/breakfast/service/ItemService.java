18bd61a650fec4dda8b4f54df0d618ac64717fa1
LuanRRLima <luan.kionda@gmail.com>
2021-10-10 07:20:54 -0300

    Ajuste no ItemService;

--- src/main/java/br/com/mv/breakfast/service/ItemService.java
+++ src/main/java/br/com/mv/breakfast/service/ItemService.java
@@ -1,5 +1,20 @@
 package br.com.mv.breakfast.service;
 
-public class ItemService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mv.breakfast.domain.Item;
+import br.com.mv.breakfast.repository.ItemRepository;
+
+@Service
+public class ItemService {
+	@Autowired
+	private ItemRepository repository;
+	
+	public Item save(Item item){
+		return this.repository.save(item);
+	}
+	
 }
